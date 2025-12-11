fab5c0e4d561de934a0841ee9aa5b76ae297c9bf
alshbcd <alshbcd@163.com>
2025-12-11 10:10:28 +0800

    Update collect_ips_mine.py

--- collect_ips_mine.py
+++ collect_ips_mine.py
@@ -6,7 +6,7 @@ import os
 
 # 目标URL列表
 urls = [
-    # 'https://ip.164746.xyz', 
+    'https://ip.164746.xyz', 
     # 'https://ip.haogege.xyz/',
     # 'https://cf.090227.xyz',
     # 'https://ct.090227.xyz',
