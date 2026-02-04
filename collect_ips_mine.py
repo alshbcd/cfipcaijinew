67dfe1bfee454b6df35f51e5048914764c892e69
alshbcd <alshbcd@163.com>
2026-02-04 10:34:45 +0800

    Update collect_ips_mine.py

--- collect_ips_mine.py
+++ collect_ips_mine.py
@@ -7,7 +7,7 @@ import os
 # 目标URL列表
 urls = [
     'https://ip.164746.xyz', 
-    'https://ip.haogege.xyz/'
+    'https://ip.haogege.xyz/',
     # 'https://cf.090227.xyz',
     # 'https://ct.090227.xyz',
     # 'https://cmcc.090227.xyz',    
@@ -19,9 +19,9 @@ urls = [
     # 'https://ipdb.api.030101.xyz/?type=cfv4;proxy',
     # 'https://ipdb.api.030101.xyz/?type=bestcf&country=true',
     # 'https://ipdb.api.030101.xyz/?type=bestproxy&country=true',
-    # 'https://www.wetest.vip/page/edgeone/address_v4.html',
-    # 'https://www.wetest.vip/page/cloudfront/address_v4.html',
-    # 'https://www.wetest.vip/page/cloudflare/address_v4.html'
+     'https://www.wetest.vip/page/edgeone/address_v4.html',
+     'https://www.wetest.vip/page/cloudfront/address_v4.html',
+     'https://www.wetest.vip/page/cloudflare/address_v4.html'
 ]
 
 # 正则表达式用于匹配IP地址
